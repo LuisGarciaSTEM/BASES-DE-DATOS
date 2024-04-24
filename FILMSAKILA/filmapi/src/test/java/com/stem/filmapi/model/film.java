c9246b1b8fd1c2b4c7c72ba5a55c72058e59c5e9
LuisGarciaSTEM <145747096+LuisGarciaSTEM@users.noreply.github.com>
2024-04-24 12:18:46 +0200

    HAcer el SGBD

---
+++ FILMSAKILA/filmapi/src/test/java/com/stem/filmapi/model/film.java
@@ -0,0 +1,58 @@
+package com.stem.filmapi.model; 
+
+public class film {
+
+    private int id;
+    private String title;
+    private String description;
+    private int length;
+
+    public film() {
+
+    }
+
+    public film(int id, String title, String description, int lenght) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.length = lenght;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "El id " + id + " titulo " + title + " la descripcion " + description + " y lenght " + length;
+    }
+
+}
